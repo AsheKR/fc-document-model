03f66f74080e251ac8a5662fd6c75e2a126871c3
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-08 00:19:34 -0700

    MTO recursivly 예제구현

--- app/foreignkey/models.py
+++ app/foreignkey/models.py
@@ -8,12 +8,27 @@ class Manufacturer(models.Model):
     def __str__(self):
         return self.name
 
+
 class Car(models.Model):
     manufacturer = models.ForeignKey(
         Manufacturer,
         on_delete=models.CASCADE,
     )
-    name= models.CharField(max_length=50)
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class FCUser(models.Model):
+    name = models.CharField(max_length=30)
+    instructor = models.ForeignKey(
+        'self',
+        blank=True,
+        null=True,
+        on_delete=models.SET_NULL,
+        related_name='students'
+    )
 
     def __str__(self):
         return self.name
