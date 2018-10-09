aadd760b46935cff23a37810fcc53e13a2de6133
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-08 21:04:25 -0700

    MTM 확장 필드(through_fields) 사용하여 테스트

--- app/many_to_many/models.py
+++ app/many_to_many/models.py
@@ -10,6 +10,7 @@ class Topping(models.Model):
 
 class Pizza(models.Model):
     name = models.CharField(max_length=50)
+    # 피자에 토핑이 들어가니까 여기 선
     toppings = models.ManyToManyField(Topping)
 
     def __str__(self):
@@ -22,3 +23,27 @@ class FacebookUser(models.Model):
 
     def __str__(self):
         return self.name
+
+class Person(models.Model):
+    name = models.CharField(max_length=120)
+
+    def __str__(self):
+        return self.name
+
+class Group(models.Model):
+    name = models.CharField(max_length=120)
+    """
+    MTMField에 두번째 인자를 주지않고 생성하면 자동으로 생성된다.
+    하지만 추가적인 필드가 필요할 때 through 키워드를 이용하면 중간 모델을 가리키도록 하여 연결 가능하다.
+    """
+    # 그룹에 사람이 들어가니까 여기 선언
+    members = models.ManyToManyField(Person, through='Membership', through_fields=('group', 'person'))
+
+    def __str__(self):
+        return self.name
+
+class Membership(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE)
+    group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    date_joined = models.DateTimeField()
+    invite_reason = models.CharField(max_length=64)
