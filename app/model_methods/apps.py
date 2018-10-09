b99d3b016641205ec6643554b73cc764bcd45f55
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-08 22:13:49 -0700

    모델 메소드 오버라디이 사용

---
+++ app/model_methods/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ModelMethodsConfig(AppConfig):
+    name = 'model_methods'
---
+++ app/model_methods/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    birth_date = models.DateField()
+
+    def baby_boomer_status(self):
+        "Returns the person's baby-boomer status"
+        import datetime
+        if self.birth_date < datetime.date(1945, 8, 1):
+            return "Pre-boomer"
+        elif self.birth_date < datetime.date(1965, 1, 1):
+            return "Baby-boomer"
+        else:
+            return "Post-boomer"
+
+    def _get_full_name(self):
+        "Returns the person's full name"
+        return '%s %s' % (self.first_name, self.last_name)
+
+    full_name = property(_get_full_name)
+
+
+class Blog(models.Model):
+    name = models.CharField(max_length=100)
+    tagline = models.TextField()
+
+    def save(self, *args, **kwargs):
+        if self.name == "Yoko Ono's blog":
+            return # Yoko shall never have her own blog!
+        else:
+            super(Blog, self).save(*args, **kwargs)
