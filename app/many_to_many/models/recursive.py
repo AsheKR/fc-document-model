e242973866d7fca238478370ae917afb7df21f84
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-09 20:07:56 -0700

    FacebookUser (MTM recursive relationships) __repr__  구현

--- app/many_to_many/models/recursive.py
+++ app/many_to_many/models/recursive.py
@@ -4,9 +4,17 @@ __all__ = (
     'FacebookUser',
 )
 
+
 class FacebookUser(models.Model):
     name = models.CharField(max_length=30)
     friends = models.ManyToManyField('self')
 
     def __str__(self):
-        return self.name
+        # 이한영 (친구: a, b, c)
+        # QuerySet 순회 및 문자열 포매팅
+        return self.name+' (친구: '+','.join([x.name for x in self.friends.all()])+')'
+
+    def __repr__(self):
+        # 이한영 (친구: a, b, c)
+        # QuerySet 순회 및 문자열 포매팅
+        return self.name+' (친구: '+','.join(self.friends.values_list('name', flat=True))+')'
