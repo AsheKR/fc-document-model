7b4ee5b5e7a5dda56f75c101c727a6d3d0e9726a
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-10 23:46:09 -0700

    abc 예제

--- app/abstract_base_classes/admin.py
+++ app/abstract_base_classes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from abstract_base_classes.models import Student
+
+admin.site.register(Student)
--- app/abstract_base_classes/models.py
+++ app/abstract_base_classes/models.py
@@ -9,12 +9,25 @@ from django.db import models
 
 
 class CommonInfo(models.Model):
-    name = models.CharField(max_length=100)
+    # DB에 넣을 때 자동으로 정렬
+    name = models.CharField(max_length=100, db_index=True)
     age = models.PositiveIntegerField()
 
+    def __str__(self):
+        return self.name
+
     class Meta:
+        # 상속하면 FALSE가 된다
         abstract = True
+        # 쿼리로 가져올 때 자동으로 정렬
+        ordering = ['name']
 
 
 class Student(CommonInfo):
     home_group = models.CharField(max_length=5)
+
+    class Meta(CommonInfo.Meta):
+        # 폼에서 보여줄 내용
+        verbose_name = '학생'
+        # 여러개일 때는 자동으로 s가 붙어서 이 옵션을 주면 이름을 바꿀 수 있다
+        verbose_name_plural = '학생 목록'
