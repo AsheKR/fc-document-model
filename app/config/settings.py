984a4356446a194560ade54ce38e8a7eb0e32013
teachmesomething2580 <teachmesomething2580@gmail.com>
2018-10-21 02:26:38 -0700

    weblog 모델 추가

--- app/config/settings.py
+++ app/config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'fields',
+    'weblog',
 
     'inheritance.abstract_base_classes',
     'inheritance.abc_other',
--- app/weblog/models.py
+++ app/weblog/models.py
@@ -3,29 +3,32 @@ from django.db import models
 
 class Blog(models.Model):
     name = models.CharField(max_length=100)
-    tagline = models.TextField(lbank=True)
+    tagline = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=200, null=True, blank=True)
     email = models.EmailField(blank=True)
 
     def __str__(self):
-        return self.name
+        if self.name:
+            return self.name
+        return 'name null'
 
 
+# 블로그의 글을 Entry라고 함
 class Entry(models.Model):
     blog = models.ForeignKey(
         Blog,
-        on_dele=models.CASCADE,
+        on_delete=models.CASCADE,
     )
     headline = models.CharField(max_length=255)
     pub_date = models.DateField(blank=True, null=True)
     authors = models.ManyToManyField(Author, blank=True)
-    n_commoent = models.IntegerField(default=0)
+    n_comment = models.IntegerField(default=0)
     n_pingbacks = models.IntegerField(default=0)
     rating = models.IntegerField(default=0)
 
